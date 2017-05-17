84d63b83a7c9ce390cc400808f07296119a5f1a6
Chris Shiels <chris@systems-engineers.co.uk>
2017-05-17 22:06:00 +0100

    Completed JavaScript implementation.

--- javascript/functional.js
+++ javascript/functional.js
@@ -8,7 +8,7 @@
 
 
 const _range = function(n, m = 0) {
-  if (n == m)
+  if (n === m)
     return [];
   else
     return _range(n - 1, m).concat(n - 1)
@@ -21,3 +21,137 @@ const _reverse = function(l) {
   else
     return _reverse(l.slice(1)).concat(l[0])
 }
+
+
+const _reduce = function(f, l, v) {
+  if (l.length === 0) {
+    return v;
+  } else
+    return _reduce(f, l.slice(1), f(v, l[0]));
+}
+
+
+const _map = function(f, l) {
+  const accumulate = function(a, e) {
+    return a.concat(f(e));
+  }
+  return _reduce(accumulate, l, []);
+}
+
+
+const _filter = function(f, l) {
+  const accumulate = function(a, e) {
+    if (f(e))
+      return a.concat(e);
+    else
+      return a;
+  }
+  return _reduce(accumulate, l, []);
+}
+
+
+const _sort = function(f, l) {
+  if (l.length === 0)
+    return [];
+  return _sort(f, _filter((e) => { return f(e, l[0]); }, l.slice(1)))
+         .concat(l[0])
+         .concat(_sort(f, _filter((e) => { return !f(e, l[0]); }, l.slice(1))));
+}
+
+
+const _unique = function(l) {
+  const accumulate = function(a, e) {
+    if (a.includes(e))
+      return a;
+    else
+      return a.concat(e);
+  }
+  return _reduce(accumulate, l, []);
+}
+
+
+const _compose = function(f, g) {
+  return function(x) {
+    return f(g(x));
+  }
+}
+
+
+const _pipe = function(l) {
+  const accumulate = function(a, e) {
+    return e(a);
+  }
+  return function(v) {
+    return _reduce(accumulate, l, v);
+  }
+}
+
+
+const _partial = function(f, ...args) {
+  let args1 = args;
+  return function(...args) {
+    return f.apply(null, args1.concat(args));
+  }
+}
+
+
+const _curry = function(f, arity) {
+  arity = arity || f.length;
+
+  return function(v) {
+    if (arity === 1)
+      return f(v);
+    else
+      return _curry(_partial(f, v), arity - 1);
+  }
+}
+
+
+const factorial = function(n) {
+  if (n <= 1)
+    return 1;
+  else
+    return n * factorial(n - 1);
+}
+
+
+const fibonacci = function(n) {
+  if (n <= 1)
+    return 1;
+  else
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+const primes = function(n) {
+  const notdivisibleby = function(y, x) {
+    return x % y !== 0;
+  }
+
+  const sieve = function(f, l) {
+    if (l.length === 0)
+      return [];
+    else
+      return [ l[0] ].concat(sieve(f, _filter(_partial(f, l[0]), l.slice(1))));
+  }
+
+  return sieve(notdivisibleby, _range(n, 2));
+}
+
+
+module.exports = {
+  '_range':    _range,
+  '_reverse':  _reverse,
+  '_reduce':   _reduce,
+  '_map':      _map,
+  '_filter':  _filter,
+  '_sort':     _sort,
+  '_unique':   _unique,
+  '_compose':  _compose,
+  '_pipe':     _pipe,
+  '_partial':  _partial,
+  '_curry':    _curry,
+  'factorial': factorial,
+  'fibonacci': fibonacci,
+  'primes':    primes
+};
---
+++ javascript/test.js
@@ -0,0 +1,244 @@
+#!/usr/bin/env node
+
+// 'test.js'.
+// Chris Shiels.
+
+
+'use strict';
+
+
+var assert = require('assert');
+var functional = require('./functional');
+
+
+describe('functional', function() {
+  describe('#_range()', function() {
+    it('returns [ 0..9 ] for _range(10)',
+       function() {
+         assert.deepEqual(functional._range(10),
+                          [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]);
+    });
+
+    it('returns [ 1..10 ] for _range(11, 1)',
+       function() {
+         assert.deepEqual(functional._range(11, 1),
+                          [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]);
+    });
+  });
+
+
+  describe('#_reverse()',function() {
+    it('returns [ 10..1 ] for _reverse(_range(11, 1))',
+       function() {
+         assert.deepEqual(functional._reverse(functional._range(11, 1)),
+                          [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]);
+    });
+  });
+
+
+  describe('#_reduce()', function() {
+    it('returns 55 for _reduce((a, e) => { return a + e; }, _range(11, 1), 0)',
+       function() {
+         assert.equal(functional._reduce((a, e) => { return a + e; },
+                                         functional._range(11, 1),
+                                         0),
+                   55);
+    });
+  });
+
+
+  describe('#_map()', function() {
+    it('returns [ 2,4,6..20 ] for _map((e) => { return e * 2; }, _range(11, 1))',
+       function() {
+         assert.deepEqual(functional._map((e) => { return e * 2; },
+                                          functional._range(11, 1)),
+                   [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ]);
+    });
+  });
+
+
+  describe('#_filter()', function() {
+    it('returns [ 2,4,6,8,10 ] for _filter((e) => { return e % 2 === 0; }, _range(11, 1))',
+       function() {
+         assert.deepEqual(functional._filter((e) => { return e % 2 === 0; },
+                          functional._range(11, 1)),
+                          [ 2, 4, 6, 8, 10 ]);
+    });
+
+    it('returns [ 1,3,5,7,9 ] for _filter((e) => { return e % 2 === 1; }, _range(11, 1))',
+       function() {
+         assert.deepEqual(functional._filter((e) => { return e % 2 === 1; },
+                          functional._range(11, 1)),
+                          [ 1, 3, 5, 7, 9 ]);
+    });
+  });
+
+
+  describe('#_sort()', function() {
+    it('returns [ 1..10 ] for _sort((m, n) => { return m <= n; }, [ 1, 10, 2, 9, 3, 8, 4, 7, 5, 6 ])',
+       function() {
+         assert.deepEqual(functional._sort((m, n) => { return m <= n; },
+                                           [ 1, 10, 2, 9, 3, 8, 4, 7, 5, 6 ]),
+                          [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]);
+    });
+  });
+
+
+  describe('#_unique()', function() {
+    it('returns [ 5,1,4,2,3 ] for _unique([ 5, 1, 4, 2, 3, 3, 2, 4, 1, 5 ])',
+       function() {
+         assert.deepEqual(functional._unique([ 5, 1, 4, 2, 3, 3, 2, 4, 1, 5 ]),
+                          [ 5, 1, 4, 2, 3 ]);
+    });
+  });
+
+
+  describe('#_compose()', function() {
+    it('returns 3 for _compose(valueadd1, valuemultiply2)(1)',
+       function() {
+         const valueadd1 = function(v) {
+           return v + 1;
+         }
+
+         const valuemultiply2 = function(v) {
+           return v * 2;
+         }
+
+         assert.equal(functional._compose(valueadd1, valuemultiply2)(1),
+                      3);
+    });
+  });
+
+
+  describe('#_pipe()', function() {
+    it('returns 5 for _pipe([ valueadd1, valuemultiply2, valueadd1 ])(1)',
+       function() {
+         const valueadd1 = function(v) {
+           return v + 1;
+         }
+
+         const valuemultiply2 = function(v) {
+           return v * 2;
+         }
+
+         assert.equal(functional._pipe([ valueadd1,
+                                         valuemultiply2,
+                                         valueadd1 ])(1),
+                      5);
+    });
+
+    it('returns [ 5,7,9,11,13,15,17,19,21,23 ] for _pipe([ listadd1, listmultiply2, listadd1 ])(_range(11, 1))',
+       function() {
+         const listadd1 = function(l) {
+           return functional._map((e) => { return e + 1; }, l);
+         }
+
+         const listmultiply2 = function(l) {
+           return functional._map((e) => { return e * 2; }, l);
+         }
+
+         assert.deepEqual(functional._pipe([ listadd1,
+                                             listmultiply2,
+                                              listadd1 ])(functional._range(11, 1)),
+                          [ 5, 7, 9, 11, 13, 15, 17, 19, 21, 23 ]);
+    });
+  });
+
+
+  describe('#_partial()', function() {
+    it('returns [ 11..20 ] for _map(_partial(add, 10), _range(11, 1))',
+       function() {
+         const add = function(a, b) {
+           return a + b;
+         }
+
+         const add10 = function(x) {
+           return add(x, 10);
+         }
+
+         assert.deepEqual(functional._map(add10,
+                                          functional._range(11, 1)),
+                          [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]);
+
+         assert.deepEqual(functional._map(functional._partial(add, 10),
+                                          functional._range(11, 1)),
+                          [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]);
+    });
+  });
+
+
+  describe('#_curry()', function() {
+    it('supports multiple execution paths',
+       function() {
+         const args = function(...a) {
+           return a;
+         }
+
+         let cargs = functional._curry(args, 5)
+         let cargsab = cargs('a')('b')
+
+         assert.deepEqual(cargsab('c')('d')('e'),
+                          [ 'a', 'b', 'c', 'd', 'e' ]);
+         assert.deepEqual(cargsab('x')('y')('z'),
+                          [ 'a', 'b', 'x', 'y', 'z' ]);
+         assert.deepEqual(cargsab('c')('d')('e'),
+                          [ 'a', 'b', 'c', 'd', 'e' ]);
+         assert.deepEqual(cargsab('x')('y')('z'),
+                          [ 'a', 'b', 'x', 'y', 'z' ]);
+    });
+
+    it('returns 15 for _curry(add, 2)(10)(5) and 25 for _curry(add, 2)(20)(5)',
+       function() {
+         const add = function(a, b) {
+           return a + b;
+         }
+
+         let cadd = functional._curry(add, 2)
+
+         assert.equal(cadd(10)(5),
+                      15);
+         assert.equal(cadd(20)(5),
+                      25);
+    });
+
+    it('returns [ 11..20 ] for _map(_curry(add, 2)(10), _range(11, 1))',
+       function() {
+         const add = function(a, b) {
+           return a + b;
+         }
+
+         assert.deepEqual(functional._map(functional._curry(add, 2)(10),
+                                      functional._range(11, 1)),
+                          [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]);
+    });
+  });
+
+
+  describe('#factorial()', function() {
+    it('returns [ 1,2,6,24,120,720,5040,40320,362880,3628800 ] for _map(factorial, _range(11, 1))',
+       function() {
+         assert.deepEqual(functional._map(functional.factorial,
+                                          functional._range(11, 1)),
+                          [ 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800 ]);
+    });
+  });
+
+
+  describe('#fibonacci()', function() {
+    it('returns [ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ] for _map(fibonacci, _range(11, 1))',
+       function() {
+         assert.deepEqual(functional._map(functional.fibonacci,
+                                          functional._range(11, 1)),
+                          [ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ]);
+    });
+  });
+
+
+  describe('#primes()', function() {
+    it('returns [ 2, 3, 5, 7, 11, 13, 17, 19 ] for primes(20)',
+       function() {
+         assert.deepEqual(functional.primes(20),
+                          [ 2, 3, 5, 7, 11, 13, 17, 19 ]);
+    });
+  });
+});
