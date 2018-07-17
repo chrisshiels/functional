38099e72103c50012abe19192bf903fffb435048
Chris Shiels <chris@systems-engineers.co.uk>
2018-07-17 16:06:40 +0100

    Removed underscore prefix from function names.

--- python/functional.py
+++ python/functional.py
@@ -4,34 +4,34 @@
 # Chris Shiels.
 
 
-def _all(f, l):
+def all(f, l):
   if len(l) == 0:
     return False
   elif len(l) == 1:
     return f(l[0])
   else:
-    return f(l[0]) and _all(f, l[1:])
+    return f(l[0]) and all(f, l[1:])
 
 
-def _any(f, l):
+def any(f, l):
   if len(l) == 0:
     return False
   elif len(l) == 1:
     return f(l[0])
   else:
-    return f(l[0]) or _any(f, l[1:])
+    return f(l[0]) or any(f, l[1:])
 
 
-def _flatten(l):
+def flatten(l):
   if l == []:
     return []
   elif type(l[0]) is list:
-    return _flatten(l[0]) + _flatten(l[1:])
+    return flatten(l[0]) + flatten(l[1:])
   else:
-    return [ l[0] ] + _flatten(l[1:])
+    return [ l[0] ] + flatten(l[1:])
 
 
-def _range(m, n = None, s = 1):
+def range(m, n = None, s = 1):
   def internal(start, stop, step):
     if step == 0 or \
        (step > 0 and start >= stop) or \
@@ -45,7 +45,7 @@ def _range(m, n = None, s = 1):
     return internal(m, n, s)
 
 
-def _reduce(f, l, v = None):
+def reduce(f, l, v = None):
   def internal(f, l, v):
     if l == []:
       return v
@@ -57,32 +57,32 @@ def _reduce(f, l, v = None):
     return internal(f, l, v)
 
 
-def _map(f, l):
+def map(f, l):
   def accumulate(a, e):
     return a + [ f(e) ]
-  return _reduce(accumulate, l, [])
+  return reduce(accumulate, l, [])
 
 
-def _filter(f, l):
+def filter(f, l):
   def accumulate(a, e):
     if f(e):
       return a + [ e ]
     else:
       return a
-  return _reduce(accumulate, l, [])
+  return reduce(accumulate, l, [])
 
 
-def _partition(f, l):
+def partition(f, l):
   def accumulate(a, e):
     left, right = a
     if f(e):
       return [ left + [ e ], right ]
     else:
       return [ left, right + [ e ] ]
-  return _reduce(accumulate, l, [ [], [] ])
+  return reduce(accumulate, l, [ [], [] ])
 
 
-def _split(f, l):
+def split(f, l):
   def accumulate(a, e):
     if f(e):
       return a + [ [] ]
@@ -91,34 +91,34 @@ def _split(f, l):
 
   def internal(e):
     return e != []
-  return _filter(internal, _reduce(accumulate, l, [ [] ]))
+  return filter(internal, reduce(accumulate, l, [ [] ]))
 
 
-def _reverse(l):
+def reverse(l):
   def accumulate(a, e):
     return [ e ] + a
-  return _reduce(accumulate, l, [])
+  return reduce(accumulate, l, [])
 
 
-def _sort(f, l):
+def sort(f, l):
   if l == []:
     return []
   else:
-    return _sort(f, _filter(lambda e: f(e, l[0]), l[1:])) + \
+    return sort(f, filter(lambda e: f(e, l[0]), l[1:])) + \
            [ l[0] ] + \
-           _sort(f, _filter(lambda e: not f(e, l[0]), l[1:]))
+           sort(f, filter(lambda e: not f(e, l[0]), l[1:]))
 
 
-def _unique(l):
+def unique(l):
   def accumulate(a, e):
     if e in a:
       return a
     else:
       return a + [ e ]
-  return _reduce(accumulate, l, [])
+  return reduce(accumulate, l, [])
 
 
-def _zip(*args):
+def zip(*args):
   def accumulateheads(a, e):
     return a + [ e[0] ]
   def accumulatetails(a, e):
@@ -126,43 +126,43 @@ def _zip(*args):
   args = list(args)
   if args[0] == []:
     return []
-  return [ _reduce(accumulateheads, args, []) ] + _zip(*_reduce(accumulatetails, args, []))
+  return [ reduce(accumulateheads, args, []) ] + \
+             zip(*reduce(accumulatetails, args, []))
 
 
-def _permutations(l):
+def permutations(l):
   if len(l) == 0:
     return []
   elif len(l) == 1:
     return [ l ]
   else:
-    return _reduce(lambda a, e1: a + _map(lambda e2: [ e1 ] + e2,
-                                          _permutations(_filter(lambda e3:
-                                                                  e3 != e1,
-                                                                l))),
-                   l,
-                   [])
+    return reduce(lambda a, e1: a + map(lambda e2: [ e1 ] + e2,
+                                        permutations(filter(lambda e3: e3 != e1,
+                                                            l))),
+                  l,
+                  [])
 
 
-def _compose(f, g):
+def compose(f, g):
   def internal(v):
     return f(g(v))
   return internal
 
 
-def _pipe(l):
+def pipe(l):
   def accumulate(a, e):
     return e(a)
 
   def internal(v):
-    return _reduce(accumulate, l, v)
+    return reduce(accumulate, l, v)
   return internal
 
 
-def _pipe2(l):
-  return _reduce(_compose, _reverse(l))
+def pipe2(l):
+  return reduce(compose, reverse(l))
 
 
-def _pipemaybe(l):
+def pipemaybe(l):
   def accumulate(a, e):
     if a != None:
       return e(a)
@@ -170,11 +170,11 @@ def _pipemaybe(l):
       return None
 
   def internal(v):
-    return _reduce(accumulate, l, v)
+    return reduce(accumulate, l, v)
   return internal
 
 
-def _partial(f, *args):
+def partial(f, *args):
   args1 = args
 
   def internal(*args):
@@ -182,18 +182,18 @@ def _partial(f, *args):
   return internal
 
 
-def _curry(f, arity = 0):
+def curry(f, arity = 0):
   arity = arity or f.__code__.co_argcount
 
   def internal(v):
     if arity == 1:
       return f(v)
     else:
-      return _curry(_partial(f, v), arity - 1)
+      return curry(partial(f, v), arity - 1)
   return internal
 
 
-def _memoize(f):
+def memoize(f):
   cache = {}
 
   def internal(*args):
@@ -226,7 +226,7 @@ def memoizedfibonacci(n):
       return 1
     else:
       return f(f, n - 1) + f(f, n - 2)
-  f = _memoize(internal)
+  f = memoize(internal)
   return f(f, n)
 
 
@@ -235,8 +235,8 @@ def primes(n):
     if l == []:
       return []
     else:
-      return [ l[0] ] + sieve(_filter(lambda e: e % l[0] != 0, l[1:]))
-  return sieve(_range(2, n))
+      return [ l[0] ] + sieve(filter(lambda e: e % l[0] != 0, l[1:]))
+  return sieve(range(2, n))
 
 
 def ispalindrome(s):
--- python/test_functional.py
+++ python/test_functional.py
@@ -6,143 +6,143 @@ import functional
 
 
 def test_all():
-  assert functional._all(lambda e: e % 2 == 0, []) == \
+  assert functional.all(lambda e: e % 2 == 0, []) == \
          False
-  assert functional._all(lambda e: e % 2 == 0, [ 2, 4, 6, 8, 10 ]) == \
+  assert functional.all(lambda e: e % 2 == 0, [ 2, 4, 6, 8, 10 ]) == \
          True
-  assert functional._all(lambda e: e % 3 == 0, [ 2, 4, 6, 8, 10 ]) == \
+  assert functional.all(lambda e: e % 3 == 0, [ 2, 4, 6, 8, 10 ]) == \
          False
 
 
 def test_any():
-  assert functional._any(lambda e: e % 2 == 0, []) == \
+  assert functional.any(lambda e: e % 2 == 0, []) == \
          False
-  assert functional._any(lambda e: e % 2 == 0, [ 2, 4, 6, 8, 10 ]) == \
+  assert functional.any(lambda e: e % 2 == 0, [ 2, 4, 6, 8, 10 ]) == \
          True
-  assert functional._any(lambda e: e % 3 == 0, [ 2, 4, 6, 8, 10 ]) == \
+  assert functional.any(lambda e: e % 3 == 0, [ 2, 4, 6, 8, 10 ]) == \
          True
 
 
 def test_flatten():
-  assert functional._flatten([]) == \
+  assert functional.flatten([]) == \
          []
-  assert functional._flatten([ 1, 2, 3 ]) == \
+  assert functional.flatten([ 1, 2, 3 ]) == \
          [ 1, 2, 3 ]
-  assert functional._flatten([ [ 1, 2 ], 3 ]) == \
+  assert functional.flatten([ [ 1, 2 ], 3 ]) == \
          [ 1, 2, 3 ]
-  assert functional._flatten([ 1, [ 2, 3 ] ]) == \
+  assert functional.flatten([ 1, [ 2, 3 ] ]) == \
          [ 1, 2, 3 ]
-  assert functional._flatten([ 1, [ 2, [ 3 ] ] ]) == \
+  assert functional.flatten([ 1, [ 2, [ 3 ] ] ]) == \
          [ 1, 2, 3 ]
-  assert functional._flatten([ [ [ 1 ], [ 2 ], [ 3 ] ] ]) == \
+  assert functional.flatten([ [ [ 1 ], [ 2 ], [ 3 ] ] ]) == \
          [ 1, 2, 3 ]
 
 
 def test_range():
-  assert functional._range(0) == \
+  assert functional.range(0) == \
          []
-  assert functional._range(10) == \
+  assert functional.range(10) == \
          [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-  assert functional._range(0, 10) == \
+  assert functional.range(0, 10) == \
          [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-  assert functional._range(0, 10, 1) == \
+  assert functional.range(0, 10, 1) == \
          [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-  assert functional._range(0, 10, 2) == \
+  assert functional.range(0, 10, 2) == \
          [ 0, 2, 4, 6, 8 ]
-  assert functional._range(0, 10, 3) == \
+  assert functional.range(0, 10, 3) == \
          [ 0, 3, 6, 9 ]
-  assert functional._range(10, 0, -1) == \
+  assert functional.range(10, 0, -1) == \
          [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
-  assert functional._range(1, 11) == \
+  assert functional.range(1, 11) == \
          [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
 
 def test_reduce():
-  assert functional._reduce(lambda a, e: a + e,
-                            functional._range(1, 11), 0) == \
+  assert functional.reduce(lambda a, e: a + e,
+                            functional.range(1, 11), 0) == \
          55
-  assert functional._reduce(lambda a, e: a + e,
-                            functional._range(1, 11)) == \
+  assert functional.reduce(lambda a, e: a + e,
+                            functional.range(1, 11)) == \
          55
-  assert functional._reduce(lambda a, e: a + e,
+  assert functional.reduce(lambda a, e: a + e,
                             [ [ 1 ], [ 2, 3 ], [ 4, 5, 6 ] ]) == \
          [ 1, 2, 3, 4, 5, 6 ]
-  assert functional._reduce(lambda a, e: a + len(e),
+  assert functional.reduce(lambda a, e: a + len(e),
                             [ [ 1 ], [ 2, 3 ], [ 4, 5, 6 ] ],
                             0) == \
          6
 
 
 def test_map():
-  assert functional._map(lambda e: e * 2,
-                         functional._range(1, 11)) == \
+  assert functional.map(lambda e: e * 2,
+                        functional.range(1, 11)) == \
          [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ]
-  assert functional._map(lambda e: e,
-                         [ [ 1 ], [ 2 ], [ 3 ] ]) == \
+  assert functional.map(lambda e: e,
+                        [ [ 1 ], [ 2 ], [ 3 ] ]) == \
          [ [ 1 ], [ 2 ], [ 3 ] ]
 
 
 def test_filter():
-  assert functional._filter(lambda e: e % 2 == 0,
-                            functional._range(1, 11)) == \
+  assert functional.filter(lambda e: e % 2 == 0,
+                           functional.range(1, 11)) == \
          [ 2, 4, 6, 8, 10 ]
-  assert functional._filter(lambda e: e % 2 == 1,
-                            functional._range(1, 11)) == \
+  assert functional.filter(lambda e: e % 2 == 1,
+                           functional.range(1, 11)) == \
          [ 1, 3, 5, 7, 9 ]
-  assert functional._filter(lambda e: len(e) != 0,
-                            [ [ 1 ], [], [ 2 ], [], [ 3 ] ]) == \
+  assert functional.filter(lambda e: len(e) != 0,
+                           [ [ 1 ], [], [ 2 ], [], [ 3 ] ]) == \
          [ [ 1 ], [ 2 ], [ 3 ] ]
 
 
 def test_partition():
-  assert functional._partition(lambda e: e % 3 == 0,
-                               functional._range(1, 11)) == \
+  assert functional.partition(lambda e: e % 3 == 0,
+                              functional.range(1, 11)) == \
          [ [ 3, 6, 9 ], [ 1, 2, 4, 5, 7, 8, 10 ] ]
-  assert functional._partition(lambda e: e % 3 != 0,
-                               functional._range(1, 11)) == \
+  assert functional.partition(lambda e: e % 3 != 0,
+                              functional.range(1, 11)) == \
          [ [ 1, 2, 4, 5, 7, 8, 10 ], [ 3, 6, 9 ] ]
-  assert functional._partition(lambda e: e == 0,
-                               functional._range(1, 11)) == \
+  assert functional.partition(lambda e: e == 0,
+                              functional.range(1, 11)) == \
          [ [], [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] ]
 
-  assert functional._partition(lambda e: len(e) > 1,
-                               [ [ 1 ], [ 1, 2 ], [ 1, 2, 3 ] ]) == \
+  assert functional.partition(lambda e: len(e) > 1,
+                              [ [ 1 ], [ 1, 2 ], [ 1, 2, 3 ] ]) == \
          [ [ [ 1, 2 ], [ 1, 2, 3 ] ], [ [ 1 ] ] ]
 
 
 def test_split():
-  assert functional._split(lambda e: e % 10 == 0,
-                           []) == \
+  assert functional.split(lambda e: e % 10 == 0,
+                          []) == \
          []
-  assert functional._split(lambda e: e % 1 == 0,
-                           functional._range(1, 11)) == \
+  assert functional.split(lambda e: e % 1 == 0,
+                          functional.range(1, 11)) == \
          []
-  assert functional._split(lambda e: e % 2 == 0,
-                           functional._range(1, 11)) == \
+  assert functional.split(lambda e: e % 2 == 0,
+                          functional.range(1, 11)) == \
          [ [ 1 ], [ 3 ], [ 5 ], [ 7 ], [ 9 ] ]
-  assert functional._split(lambda e: e % 3 == 0,
-                           functional._range(1, 11)) == \
+  assert functional.split(lambda e: e % 3 == 0,
+                          functional.range(1, 11)) == \
          [ [ 1, 2 ], [ 4, 5 ], [ 7, 8 ], [ 10 ] ]
 
 
 def test_reverse():
-  assert functional._reverse(functional._range(1, 11)) ==  \
+  assert functional.reverse(functional.range(1, 11)) ==  \
          [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
-  assert functional._reverse([ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ] ]) == \
+  assert functional.reverse([ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ] ]) == \
          [ [ 5, 6 ], [ 3, 4 ], [ 1, 2 ] ]
 
 
 def test_sort():
-  assert functional._sort(lambda m, n: m <= n,
-                          [1, 10, 2, 9, 3, 8, 4, 7, 5, 6 ]) == \
-         functional._range(1, 11)
-  assert functional._sort(lambda m, n: len(m) <= len(n),
-                          [ [ 1, 2, 3], [ 1, 2 ], [ 1 ] ]) == \
-                          [ [ 1 ], [ 1, 2 ],  [ 1, 2, 3 ] ]
+  assert functional.sort(lambda m, n: m <= n,
+                         [1, 10, 2, 9, 3, 8, 4, 7, 5, 6 ]) == \
+         functional.range(1, 11)
+  assert functional.sort(lambda m, n: len(m) <= len(n),
+                         [ [ 1, 2, 3], [ 1, 2 ], [ 1 ] ]) == \
+                         [ [ 1 ], [ 1, 2 ],  [ 1, 2, 3 ] ]
 
 
 def test_unique():
-  assert functional._unique([ 5, 1, 4, 2, 3, 3, 2, 4, 1, 5 ]) == \
+  assert functional.unique([ 5, 1, 4, 2, 3, 3, 2, 4, 1, 5 ]) == \
          [ 5, 1, 4, 2, 3 ]
 
 
@@ -153,26 +153,26 @@ def test_zip():
          [ 2, 6 ],
          [ 3, 7 ],
          [ 4, 8 ] ]
-  assert functional._zip(*a) == \
+  assert functional.zip(*a) == \
          a1
-  assert functional._zip(*functional._zip(*a)) == a
+  assert functional.zip(*functional.zip(*a)) == a
 
 
 def test_permutations():
-  assert functional._permutations([]) == \
+  assert functional.permutations([]) == \
          []
-  assert functional._permutations([ 1 ]) == \
+  assert functional.permutations([ 1 ]) == \
          [ [ 1 ] ]
-  assert functional._permutations([ 1, 2 ]) == \
+  assert functional.permutations([ 1, 2 ]) == \
          [ [ 1, 2 ], [ 2, 1 ] ]
-  assert functional._permutations([ 1, 2, 3 ]) == \
+  assert functional.permutations([ 1, 2, 3 ]) == \
          [ [ 1, 2, 3 ],
            [ 1, 3, 2 ],
            [ 2, 1, 3 ],
            [ 2, 3, 1 ],
            [ 3, 1, 2 ],
            [ 3, 2, 1 ] ]
-  assert functional._permutations([ 1, 2, 3, 4 ]) == \
+  assert functional.permutations([ 1, 2, 3, 4 ]) == \
          [ [ 1, 2, 3, 4 ],
            [ 1, 2, 4, 3 ],
            [ 1, 3, 2, 4 ],
@@ -206,7 +206,7 @@ def test_compose():
   def valuemultiply2(v):
     return v * 2
 
-  assert functional._compose(valueadd1, valuemultiply2)(1) == 3
+  assert functional.compose(valueadd1, valuemultiply2)(1) == 3
 
 
 def test_pipe():
@@ -217,17 +217,17 @@ def test_pipe():
     return v * 2
 
   def listadd1(l):
-    return functional._map(lambda e: e + 1, l)
+    return functional.map(lambda e: e + 1, l)
 
   def listmultiply2(l):
-    return functional._map(lambda e: e * 2, l)
+    return functional.map(lambda e: e * 2, l)
 
-  assert functional._pipe([ valueadd1,
-                            valuemultiply2,
-                            valueadd1 ])(1) == 5
-  assert functional._pipe([ listadd1,
-                            listmultiply2,
-                            listadd1 ])(functional._range(1, 11)) == \
+  assert functional.pipe([ valueadd1,
+                           valuemultiply2,
+                           valueadd1 ])(1) == 5
+  assert functional.pipe([ listadd1,
+                           listmultiply2,
+                           listadd1 ])(functional.range(1, 11)) == \
          [ 5, 7, 9, 11, 13, 15, 17, 19, 21, 23 ]
 
 
@@ -239,17 +239,17 @@ def test_pipe2():
     return v * 2
 
   def listadd1(l):
-    return functional._map(lambda e: e + 1, l)
+    return functional.map(lambda e: e + 1, l)
 
   def listmultiply2(l):
-    return functional._map(lambda e: e * 2, l)
+    return functional.map(lambda e: e * 2, l)
 
-  assert functional._pipe2([ valueadd1,
-                            valuemultiply2,
-                            valueadd1 ])(1) == 5
-  assert functional._pipe2([ listadd1,
-                            listmultiply2,
-                            listadd1 ])(functional._range(1, 11)) == \
+  assert functional.pipe2([ valueadd1,
+                           valuemultiply2,
+                           valueadd1 ])(1) == 5
+  assert functional.pipe2([ listadd1,
+                           listmultiply2,
+                           listadd1 ])(functional.range(1, 11)) == \
          [ 5, 7, 9, 11, 13, 15, 17, 19, 21, 23 ]
 
 
@@ -260,12 +260,12 @@ def test_pipemaybe():
   def valuenone(v):
     return None
 
-  assert functional._pipemaybe([ valueadd1,
-                                 valueadd1,
-                                 valueadd1 ])(0) == 3
-  assert functional._pipemaybe([ valueadd1,
-                                 valuenone,
-                                 valueadd1 ])(0) == None
+  assert functional.pipemaybe([ valueadd1,
+                                valueadd1,
+                                valueadd1 ])(0) == 3
+  assert functional.pipemaybe([ valueadd1,
+                                valuenone,
+                                valueadd1 ])(0) == None
 
 
 def test_partial():
@@ -275,11 +275,11 @@ def test_partial():
   def add10(x):
     return add(x, 10)
 
-  assert functional._map(add10,
-                         functional._range(1, 11)) == \
+  assert functional.map(add10,
+                        functional.range(1, 11)) == \
          [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
-  assert functional._map(functional._partial(add, 10),
-                         functional._range(1, 11)) == \
+  assert functional.map(functional.partial(add, 10),
+                        functional.range(1, 11)) == \
          [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
 
 
@@ -287,7 +287,7 @@ def test_curry():
   def args(*a):
     return a
 
-  cargs = functional._curry(args, 5)
+  cargs = functional.curry(args, 5)
   cargsab = cargs('a')('b')
   assert cargsab('c')('d')('e') == ('a', 'b', 'c', 'd', 'e')
   assert cargsab('x')('y')('z') == ('a', 'b', 'x', 'y', 'z')
@@ -298,21 +298,21 @@ def test_curry():
   def add(a, b):
     return a + b
 
-  cadd = functional._curry(add)
+  cadd = functional.curry(add)
   assert cadd(10)(5) == 15 and cadd(20)(5) == 25
 
-  assert functional._map(functional._curry(add)(10),
-                         functional._range(1, 11)) == \
+  assert functional.map(functional.curry(add)(10),
+                        functional.range(1, 11)) == \
          [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
 
 
 def test_factorial():
-  assert functional._map(functional.factorial, functional._range(1, 11)) == \
+  assert functional.map(functional.factorial, functional.range(1, 11)) == \
          [ 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800 ]
 
 
 def test_fibonacci():
-  assert functional._map(functional.fibonacci, functional._range(1, 11)) == \
+  assert functional.map(functional.fibonacci, functional.range(1, 11)) == \
          [ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ]
 
 
